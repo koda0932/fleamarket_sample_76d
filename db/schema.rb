a86e71dd866a25ce5523b343606690e7fc88e6d8
kantapro2020 <kantapro2020@gmail.com>
2020-06-11 10:12:53 +0900

    コメントアウト削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_062343) do
+ActiveRecord::Schema.define(version: 2020_06_09_014407) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
