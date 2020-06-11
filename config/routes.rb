c669cdfcd1dd279585005f4e90c1dd6dbe7d5784
aki-tech <gogoakito666@gmail.com>
2020-06-11 12:01:21 +0900

    user-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -7,4 +8,8 @@ Rails.application.routes.draw do
     post 'user_address', to: 'users/registrations#create_address'
   end
   root 'posts#index'
+  resources :users, only: [:show, :destroy]
+  get '/mypage' => 'users#mypage'
+  get '/mypage/destroy' => 'users#destroy'
+  resources :cards, only: [:new]
 end
