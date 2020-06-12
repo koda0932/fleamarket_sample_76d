a0f55a4c0d9ea005b622eba1da630a5c02d1c558
kantapro2020 <kantapro2020@gmail.com>
2020-06-12 18:39:28 +0900

    コメントアウト削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,18 +14,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    # if @post.post_images.present?
+
       if @post.save
         redirect_to root_path
       else
-        # render partial:"form" ,locals: {post:new}
+
         render action: :new
       end
-    # else
-      # render partial:"form" ,locals: {post:new}
-      # render :new
-      # render action: :new
-    # end
+
   end
 
   def edit
@@ -44,35 +40,6 @@ class PostsController < ApplicationController
     end
   end
 
-#   def update
-#     @parents = Category.where(ancestry: nil)
-    
-#     if params[:post].keys.include?("image") || params[:post].keys.include?("images_attributes")
-#       if @post.valid?
-#         if params[:post].keys.include?("image")
-#           update_images_ids = params[:post][:image].values
-#           before_images_ids = @post.post_images.ids
-
-#           before_images_ids.each do |before_img_id|
-#             Image.find(before_img_id).destroy unless update_images_ids.include?("#{before_img_id}")
-#           end
-#         else
-#           before_images_ids.each do |before_img_id|
-#             Image.find(before_img_id).destroy 
-#           end
-#       end
-
-#       @post.update(post_params)
-#       # @size = @post.categories[1].sizes[0]
-#       # @post.update(size: nil) unless @size
-#       redirect_to root_path, notice: "更新しました"
-#     else
-#       render 'edit'
-#     end
-#   else
-#     redirect_back(fallback_location: root_path,flash: {success: '画像がありません'})
-#   end
-# end
 
   def destroy
     if @post.destroy
@@ -94,7 +61,6 @@ class PostsController < ApplicationController
 
   def set_parents
     @parents = Category.where(ancestry: nil)
-    # @parents = Category.all.order("id asc").limit(13)
   end
 
 end
