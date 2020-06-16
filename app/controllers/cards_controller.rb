d148d35c972a1fd1fa57f310f52d49d69b511f48
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-06-16 17:14:29 +0900

    コンフリクトの解消と不要な記述の削除

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,19 +1,10 @@
 class CardsController < ApplicationController
   skip_before_action :verify_authenticity_token
-<<<<<<< Updated upstream
   before_action :set_payjp_key, except: :new
 
   def index
     if current_user.card.present?
       @cards = Card.where(user_id: current_user.id)
-=======
-
-  def index
-    if current_user.card.present?
-      @cards = current_user.card
-      # @customer = Payjp::Customer.retrieve(@card.customer_id)
-      # @default_card_information = @customer.cards.retrieve(@card.card_id)
->>>>>>> Stashed changes
     end
   end
 
@@ -21,7 +12,6 @@ class CardsController < ApplicationController
   end
 
   def create
-<<<<<<< Updated upstream
     if params[:payjp_token]
       customer = Payjp::Customer.create(card: params[:payjp_token])
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
@@ -42,28 +32,6 @@ class CardsController < ApplicationController
 
   def set_payjp_key
     Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-=======
-    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-    if params[:payjp_token].blank?
-      render :new
-    else 
-      customer = Payjp::Customer.create(card: params[:payjp_token])
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "index"
-      else
-        render :new
-      end
-    end
-  end
-
-  def show
-  end
-
-
-  def destroy
-
->>>>>>> Stashed changes
   end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,22 +1,3 @@
 class Card < ApplicationRecord
   belongs_to :user, optional: true
-
-  # require 'payjp'
-  #   Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-
-  # def self.create_card_to_payjp(params)
-  #   #トークンを作成
-  #   token = Payjp::Token.create({
-  #     card: {
-  #       number:     params['number'],
-  #       cvc:        params['cvc'],
-  #       exp_month:  params['exp_month'],
-  #       exp_year:   params['exp_year']
-  #     }},
-  #     {'X-Payjp-Direct-Token-Generate': 'true'} 
-  #   )
-  #   # 上記で作成したトークンをもとに顧客情報を作成
-  #   Payjp::Customer.create(card: token.id)
-  # end
-
 end
