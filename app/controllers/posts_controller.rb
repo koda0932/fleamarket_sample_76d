47e6e9b8ef090e2284af98cc989dc708fea9c97d
koda0932 <kyama8209@gmail.com>
2020-06-16 16:17:07 +0900

    Merge branch 'master' into new_post_layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -77,6 +77,18 @@ end
     end
   end
   
+  def search
+    respond_to do |format|
+      format.html
+      format.json do
+        if params[:parent_id]
+          @childrens = Category.find(params[:parent_id]).children
+        elsif params[:children_id]
+          @grandChilds = Category.find(params[:children_id]).children
+        end
+      end
+    end
+  end
 
   def search
     respond_to do |format|
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!, only: [:mypage]
+
+  def mypage
+  end
+
+  def logout
+  end
+
+  private
+
 end
