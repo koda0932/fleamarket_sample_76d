0995202d8c12c061c3f4e1b2f3a4d4b95cc440fb
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-08-11 09:44:39 +0900

    add: カテゴリー一覧を追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,42 +9,14 @@ class CategoriesController < ApplicationController
     @all_posts = Post.includes([:post_images, :category])
     # postsを1個ずつ取り出す。category_idかparent_idかroot_idが同じなら配列postsに代入
     @all_posts.each do |post|
-      # if post.category.id == @category.id
-      #   @posts << post
-      # elsif post.category.parent_id == @category.id
-      #   @posts << post
-      # elsif post.category.root_id == @category.id
-      #   @posts << post
-      # end
-
-      case post.category_id
-      when post.category_id == @category.id
+      if post.category.id == @category.id
         @posts << post
-      when post.category.parent_id == @category.id
+      elsif post.category.parent_id == @category.id
         @posts << post
-      when true
+      elsif post.category.root_id == @category.id
         @posts << post
-        binding.pry
-      else
-        @p = post.category.root
-        @c = post.category.parent
-        @g = post.category
       end
-
     end
-
-        # case post
-        # when post.category.id == @category.id
-        #   @posts << post
-        # when post.category.parent_id == @category.id
-        #   @posts << post
-        # when post.category.root_id == @category.id
-        #   @posts << post
-        #   binding.pry
-        # else
-        #   @p = post.category.root
-        #   @c = post.category.parent
-        #   @g = post.category
-
   end
+
 end
