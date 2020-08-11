175caa248f2833e1aceffe654a28878ae1fe03bd
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-08-11 11:32:30 +0900

    add: カテゴリー一覧のマークアップ

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,14 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.all.order('created_at asc')
+    @parents = Category.where(ancestry: nil)
   end
 
   def show
     @category = Category.find(params[:id])
     @posts = []
     @all_posts = Post.includes([:post_images, :category])
-    # postsを1個ずつ取り出す。category_idかparent_idかroot_idが同じなら配列postsに代入
+    # postsを1個ずつ取り出す。idかparent_idかroot_idが、変数@category.idと同じなら配列@postsに代入
     @all_posts.each do |post|
       if post.category.id == @category.id
         @posts << post
